761d349b34057039c561df7d61a8bfecd728f63c
DonovanTarsis <donovan.engenharia@hotmail.com>
2022-06-15 19:04:15 -0300

    API admin pronta

--- src/main/java/com/thekubernetes/apiadmin/service/TokenServiceImpl.java
+++ src/main/java/com/thekubernetes/apiadmin/service/TokenServiceImpl.java
@@ -1,6 +1,5 @@
 package com.thekubernetes.apiadmin.service;
 
-import com.thekubernetes.apiadmin.exception.ApiRequestException;
 import com.thekubernetes.apiadmin.security.Token;
 import com.thekubernetes.apiadmin.security.TokenUtil;
 import org.springframework.security.core.Authentication;
@@ -15,14 +14,13 @@ public class TokenServiceImpl implements ITokenService {
     public ResponseEntity<Object> ValidToken(Token token) {
         String tokenString = token.getToken();
         if (token.getToken() == null) {
-            return ResponseEntity.status(400).body("{\"message\":\"Campo 'Token' precisa ser informado.\"}");
+            return ResponseEntity.status(400).body("{\"message\":\"Campo 'token' precisa ser informado.\"}");
       }
       try {
         Authentication validacao = TokenUtil.validateTokenUtil(tokenString);
           return ResponseEntity.status(201).body(validacao);
       } catch (Exception e) {
-
-          throw new ApiRequestException("Não foi possível Cadastrar admin.");
+        return ResponseEntity.status(403).body("{\"message\":\"Não autorizado\"}");
       }
     }
 
