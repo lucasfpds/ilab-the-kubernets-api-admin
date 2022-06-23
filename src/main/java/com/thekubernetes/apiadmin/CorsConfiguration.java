093c3c367614deb897d170521a3e0d84feb636b8
DonovanTarsis <donovan.engenharia@hotmail.com>
2022-06-23 15:35:22 -0300

    Fix Cors

--- src/main/java/com/thekubernetes/apiadmin/CorsConfiguration.java
+++ src/main/java/com/thekubernetes/apiadmin/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(System.getenv("CORS_ADMIN_URL"))
+                .allowedOrigins(System.getenv("CORS_URL"))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
